0d464be13fce9d7118abf53c1472e16f67f6baa9
Jesus Santos <jo_nez@hotmail.com>
2023-08-27 10:39:53 -0700

    incl tbl mod, view & +role wrk, min chg to schema

--- db/seeds.sql
+++ db/seeds.sql
@@ -7,13 +7,13 @@ VALUES  (1, 'Executive'),
 
 INSERT INTO role (id, title, salary, department_id)
 VALUES  (1, 'Chief Executive Officer', 250000000.00, 1),
-        (2, 'Chief Human Resources Officer', 125000000.00, 2),
-        (3, 'Chief Marketing Officer', 150000000.00, 3),
-        (4, 'Chief Financial Officer', 200000000.00, 4),
-        (5, 'Chief Technology Officer', 200000000.00, 5);
+        (2, 'Chief Human Resources Officer', 125000000.00, 1),
+        (3, 'Chief Marketing Officer', 150000000.00, 1),
+        (4, 'Chief Financial Officer', 200000000.00, 1),
+        (5, 'Chief Technology Officer', 200000000.00, 1);
 
 INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
-VALUES  (1, 'Ximena', 'Ordonez', 1),
+VALUES  (1, 'Ximena', 'Ordonez', 1, NULL),
         (2, 'Chiranjivi', 'Chand', 2, 1),
         (3, 'Nakahara', 'Mayumi', 3, 1),
         (4, 'Aaminah', 'Hameed', 4, 1),
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 /* IMPORT MODULES */
 const connection = require("./config/connection");
 const inquirer = require("inquirer");
+const Table = require("easy-table");
 
 /* CONNECT TO DATABASE */
 connection.connect(function (err) {
@@ -27,7 +28,6 @@ const mainMenu = () => {
                 "Update an employee role",
                 "Exit",
             ],
-            default: "Exit",
         },
     ])
         .then((answer) => {
@@ -54,19 +54,122 @@ const mainMenu = () => {
 /* INTERACTIVE FUNCTIONS */
 /* VIEW ALL DEPARTMENTS */
 const viewDepartments = () => {
-    connection.query("SELECT id AS ID, name AS NAME FROM department", function (err, res) {
+    connection.query("SELECT id AS ID, name AS Name FROM department", function (err, res) {
         if (err) throw err;
-        console.table(res, ["ID", "NAME"]);
+        console.log("");
+        console.log(Table.print(res));
+        console.log("");
         mainMenu();
     });
 };
 
 /* VIEW ALL ROLES */
 const viewRoles = () => {
+    connection.query("SELECT role.id AS ID, role.title AS Title, department.name AS Department, CONCAT('$',FORMAT(role.salary, 2)) AS Salary FROM role JOIN department ON department.id = role.department_id", function (err, res) {
+        if (err) throw err;
+        console.log("");
+        console.log(Table.print(res));
+        console.log("");
+        mainMenu();
+    });
+};
+
+/* VIEW ALL EMPLOYEES */
+const viewEmployees = () => {
+    connection.query("SELECT e.id AS ID, e.first_name AS First_Name, e.last_name AS Last_Name, role.title AS Title, department.name AS Department, CONCAT('$',FORMAT(role.salary, 2)) AS Salary, CONCAT(m.first_name, ' ', m.last_name) AS Manager FROM employee AS e LEFT JOIN role ON role.id = e.role_id LEFT JOIN department ON department.id = role.department_id LEFT JOIN employee AS m ON m.id = e.manager_id", function (err, res) {
+        if (err) throw err;
+        console.log("");
+        console.log(Table.print(res));
+        console.log("");
+        mainMenu();
+    });
+};
+
+/* ADD A DEPARTMENT */
+const addDepartment = () => {
+    inquirer.prompt([
+        {
+            name: 'newDepartmentName',
+            type: 'input',
+            message: 'Enter the name of the new department:'
+        }
+    ])
+        .then((answer) => {
+            connection.query("INSERT INTO department SET?", {
+                name: answer.newDepartmentName
+            },
+                function (err) {
+                    if (err) throw err;
+                    console.log("");
+                    console.log("Department added!");
+                    console.log("");
+                    viewDepartments();
+                });
+        });
+};
+
+/* ADD A ROLE */
+const addRole = () => {
+    let deptNameArray = [];
+    connection.query("SELECT * FROM department", function (err, res) {
+        if (err) throw err;
+        res.forEach(element => {
+            deptNameArray.push(element.name);
+        });
+    });
+    inquirer.prompt([
+        {
+            name: 'newRoleTitle',
+            type: 'input',
+            message: 'Enter the title of the new role:'
+        },
+        {
+            name: 'newRoleSalary',
+            type: 'input',
+            message: 'Enter the salary of the new role:'
+        },
+        {
+            name: 'newRoleDepartment',
+            type: 'list',
+            message: 'Select the department the new role belongs to:',
+            choices: deptNameArray
+        },
+    ])
+        .then((answer) => {
+            let departments;
+            connection.query("SELECT * FROM department WHERE name =?", answer.newRoleDepartment, function (err, res) {
+                if (err) throw err;
+                departments = res;
+                connection.query("INSERT INTO role SET?", {
+                    title: answer.newRoleTitle,
+                    salary: answer.newRoleSalary,
+                    department_id: departments[0].id
+                },
+                    function (err) {
+                        if (err) throw err;
+                    });
+                console.log("");
+                console.log("Role added!");
+                console.log("");
+            });
+            mainMenu();
+        });
+};
+
+/* ADD AN EMPLOYEE */
+const addEmployee = () => {
     connection.query("SELECT id AS ID, title AS TITLE, salary AS SALARY FROM role", function (err, res) {
         if (err) throw err;
-        console.log(res);
-        // console.table(res, ["ID", "TITLE", "SALARY"]);
+        console.table(res);
+        mainMenu();
+    });
+};
+
+/* VIEW ALL ROLES */
+const updateEmployeeRole = () => {
+    connection.query("SELECT id AS ID, title AS TITLE, salary AS SALARY FROM role", function (err, res) {
+        if (err) throw err;
+        console.table(res);
         mainMenu();
     });
 };
